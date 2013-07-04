a870e0449040c5e1cdf83d6167db4a75ae65a811
qwertyV112 <streloc_pm@mail.ru>
2013-07-04 10:11:50 +0600

    Лог10

---
+++ Calculator/Log10_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Log10 : operation
+    {
+        public override double aaaaa(double x)
+        {
+            return Math.Log10(x);
+        }
+    }
+}
