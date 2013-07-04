2a5c07d5020c3b32708c4c2fbda6990c16ae8c43
NataStepanova <nata_stepanov@mail.ru>
2013-07-04 09:58:59 +0600

    Аргтангенс

---
+++ Calculator/Atg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Atg: operation
+    {
+        public override double aaaaa(double x)
+        {
+            return Math.Atan(x);
+        }
+    }
+}
