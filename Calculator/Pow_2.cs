1000db82c8affe208b16f6ca249846acb434a9b3
qwertyV112 <streloc_pm@mail.ru>
2013-07-04 10:14:50 +0600

    pow

---
+++ Calculator/Pow_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Pow : BinaryOperation
+    {
+        public override double bbbbbb(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+    }
+}
