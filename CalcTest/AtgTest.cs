f6b227d4fa07795c3dd94f03a1566be7bfe6b605
qwertyV112 <streloc_pm@mail.ru>
2013-07-05 09:48:56 +0600

    Арктангенс Тест

---
+++ CalcTest/AtgTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class AtgTest
+    {
+        [TestMethod]
+        public void AtgTest1()
+        {
+            Operation calc = MonoFabric.Calc("atg");
+            double rez1 = calc.Calculate(1);
+            Assert.AreEqual(0.785, rez1, 0.001, "Ошибка");
+            double rez2 = calc.Calculate(-2);
+            Assert.AreEqual(-1.107, rez2, 0.001, "Ошибка");
+            double rez3 = calc.Calculate(98765432);
+            Assert.AreEqual(1.57, rez3, 0.001, "Ошибка");
+        }
+    }
+}
