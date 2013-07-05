683f7906259b1dff3d6b3b7fd9aa7cb661c72b03
qwertyV112 <streloc_pm@mail.ru>
2013-07-05 09:49:57 +0600

    Тангенс Тест

---
+++ CalcTest/TangensTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class TangensTest
+    {
+        [TestMethod]
+        public void TangensTest1()
+        {
+            Operation calc = MonoFabric.Calc("tg");
+            double rez1 = calc.Calculate(9);
+            Assert.AreEqual(0, rez1, 0.001, "Ошибка");
+            double rez2 = calc.Calculate(180);
+            Assert.AreEqual(0, rez2, 0.001, "Ошибка");
+            double rez3 = calc.Calculate(30);
+            Assert.AreEqual(1, rez3, 0.001, "Ошибка");
+            double rez4 = calc.Calculate(0);
+            Assert.AreEqual(0, rez4, 0.001, "Ошибка");
+            double rez5 = calc.Calculate(45);
+            Assert.AreEqual(1, rez5, 0.001, "Ошибка");
+        }
+    }
+}
