da749e6580223b04212500d8de863e37ca30a501
VladlenaDevyatyarova <mc-schastie@rambler.ru>
2013-07-05 09:54:59 +0600

    Косинус тест

---
+++ CalcTest/CosTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class CosTest
+    {
+        [TestMethod]
+        public void CosTest1()
+        {
+            Operation calc = MonoFabric.Calc("cos");
+            double rez1 = calc.Calculate(0.5);
+            Assert.AreEqual(0.8775, rez1, 0.0001, "Ошибка");
+            double rez2 = calc.Calculate(-2);
+            Assert.AreEqual(-0.416, rez2, 0.001, "Ошибка");
+            double rez3 = calc.Calculate(98765432);
+            Assert.AreEqual(-0.252, rez3, 0.001, "Ошибка");
+        }
+    }
+}
