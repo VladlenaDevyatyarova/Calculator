286b008e8a444d04ac62b874e0cb1ee67dc97e70
VladlenaDevyatyarova <mc-schastie@rambler.ru>
2013-07-05 09:54:16 +0600

    Степень Тест

---
+++ CalcTest/PowTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class PowTest
+    {
+        [TestMethod]
+        public void PowTest1()
+        {
+            BinaryOperation calc = fabric.Calc("x^y");
+            double rez1 = calc.Calculate(2, 1);
+            Assert.AreEqual(2, rez1, 0.001, "Ошибка");
+            double rez2 = calc.Calculate(8, 4);
+            Assert.AreEqual(4096, rez2, 0.001, "Ошибка");
+        }
+    }
+}
