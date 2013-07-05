faa5085e1bbd8c15e916249377a483dc0738805b
qwertyV112 <streloc_pm@mail.ru>
2013-07-05 09:51:05 +0600

    Лн Тест

---
+++ CalcTest/LnTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class LnTest
+    {
+        [TestMethod]
+        public void LnTest1()
+        {
+            Operation calc = MonoFabric.Calc("ln");
+            double rez1 = calc.Calculate(1);
+            Assert.AreEqual(0, rez1, 0.001, "Ошибка");
+            double rez2 = calc.Calculate(-2);
+            Assert.AreEqual(0, rez2, 0.001, "Ошибка");
+            double rez3 = calc.Calculate(98765432);
+            Assert.AreEqual(18.408, rez3, 0.001, "Ошибка");
+        }
+    }
+}
