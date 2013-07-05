f8156615832f2ff6eaa175e9df70468aa446099d
VladlenaDevyatyarova <mc-schastie@rambler.ru>
2013-07-05 09:53:34 +0600

    Вычитание тест

---
+++ CalcTest/VichitanieTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class VichitanieTest
+    {
+        [TestMethod]
+        public void VichitanieTest1()
+        {
+            BinaryOperation calc = fabric.Calc("-");
+            double rez1 = calc.Calculate(2, 1);
+            Assert.AreEqual(1, rez1, 0.001, "Ошибка");
+            double rez2 = calc.Calculate(5.98, 10.43);
+            Assert.AreEqual(-4.45, rez2, 0.001, "Ошибка");
+        }
+    }
+}
