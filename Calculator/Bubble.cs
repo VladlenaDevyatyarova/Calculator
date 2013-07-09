f72d1f6244e68d6599d51c354de48c2b6f735227
VladlenaDevyatyarova <mc-schastie@rambler.ru>
2013-07-09 15:46:54 +0700

    Сортировка пузырьком

---
+++ Calculator/Bubble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Bubble : StringOperation
+    {
+        public override int[] Calculate(int[] StringInt)
+        {
+            int temp = 0;
+            for (int i = 0; i < StringInt.Length - 1; i++)
+            {
+                for (int j = i + 1; j < StringInt.Length; j++)
+                {
+                    if (StringInt[i] > StringInt[j])
+                    {
+                        temp = StringInt[i];
+                        StringInt[i] = StringInt[j];
+                        StringInt[j] = temp;
+                    }
+                }
+            }
+            return StringInt;
+        }
+    }
+}
+
